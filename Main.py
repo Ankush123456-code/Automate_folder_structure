35c3799547ad17663c89e5d935acd70d77436de9
ankushkumar1840 <ankushkumar1840@gmail.com>
2022-12-13 15:12:30 +0530

    modified

--- Main.py
+++ Main.py
@@ -2,11 +2,7 @@ from File_automate import app
 
 
 if __name__ == "__main__":
-    
-    basepath = "C:/Users/Ankush babu/Desktop/webinar/"
-    
+
+    basepath = input("Enter your base directory path")
     file_name = app.Arrange_file(basepath=basepath)
-    if len(file_name) > 0:
-        print(file_name)
-    else:
-        print("Failed")
+    print(file_name)
